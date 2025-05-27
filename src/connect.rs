c91d5377f7d7e3edb466e3fc6b4e8247c45e3101
RustDesk <71636191+rustdesk@users.noreply.github.com>
2025-05-27 21:58:36 +0800

    Update connect.rs

--- src/connect.rs
+++ src/connect.rs
@@ -879,7 +879,7 @@ impl Service<Uri> for ConnectorService {
     }
 
     fn call(&mut self, dst: Uri) -> Self::Future {
-        log::debug!("starting new connection: {dst:?}");
+        // log::debug!("starting new connection: {dst:?}");
         let timeout = self.simple_timeout;
 
         // Local transports (UDS) skip proxies
